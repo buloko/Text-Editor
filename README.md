ec3ca4ff8da4a8929db30cec2172ad77b420ba75
buloko <buloko8@gmail.com>
2023-03-26 01:12:07 -0400

    editing the reademe and adding a service worker

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Text-Editor
+This weeks challenge focused on our abilites to implement webpack into your our application and also creating an installable version that works offline.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require("workbox-webpack-plugin")
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -30,8 +31,9 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-
+      
       // manifest.json
+      new WorkboxPlugin.GenerateSW(),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
